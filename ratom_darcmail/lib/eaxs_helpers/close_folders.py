72dee8034124ab3364c2af490cd6bf8b94bc8ad4
Nitin Arora <nitaro74@gmail.com>
2019-04-30 13:55:20 -0400

    Added actual sub-package files this time!

---
+++ ratom_darcmail/lib/eaxs_helpers/close_folders.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+
+""" ???
+
+Todo:
+    * Check input types?
+    * Add logging?
+"""
+
+
+def main(folder, opened_folders):
+    """ Looks at the current @folder depth in the context of previously @openen_folders and 
+    returns a list of folders to close. Designed to accomodate EAXS' use of recursive <Folder>
+    elements.
+    
+    Args:
+        - folder (lib.folder_object.FolderObject): The most recently opened folder within the
+        EAXS context.
+        - opened_folders (list): The list of non-closed folders within the EAXS context.
+    
+    Returns:
+        list: The return value.
+        Each item in the list is a lib.folder_object.FolderObject that needs to be immediately 
+        closed within the EAXS context.
+    """
+
+    # assume there are no folders to close.
+    folders_to_close = []
+
+    # if the last item in @opened_folders is not a parent of @folder then add it to 
+    # @folders_to_close and remove it from @opened_folders.
+    for opened in reversed(opened_folders):
+        if not folder.path.startswith(opened.path):
+            folders_to_close.append(opened)
+            opened_folders.remove(opened)
+    
+    # because @folder is still opened, add it to @opened_folders.
+    opened_folders.append(folder)
+
+    return folders_to_close
+
+
+if __name__ == "__main__":
+    pass
